4d5fb57d529c66c87e091a64fec40216fd9a3779
Hedelvan <hedelvan.sitema@gmail.com>
2022-08-09 19:39:20 -0500

    Update calculadora.py
    closed #2

--- calculadora.py
+++ calculadora.py
@@ -3,4 +3,16 @@ number_2 = int(input('Enter your second number: '))
 
 print('{} + {} = '.format(number_1, number_2))
 print(number_1+number_2)
-#
+
+#Subtraction
+
+print('{} - {} = '.format(number_1, number_2))
+print(number_1-number_2)
+
+#Mutiplication
+print('{} * {} = '.format(number_1, number_2))
+print(number_1*number_2)
+
+#Division
+print('{} / {} = '.format(number_1, number_2))
+print(number_1/number_2)
