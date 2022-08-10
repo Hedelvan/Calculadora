fe2fb27afa6cdb0e2548c99fc505209c05becd3f
Hedelvan <hedelvan.sitema@gmail.com>
2022-08-09 19:52:39 -0500

    Update calculadora.py
    closed #4

--- calculadora.py
+++ calculadora.py
@@ -1,25 +1,40 @@
-operation = input('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for subtracion
-* for multiplication
-/ for division
-''')
+def calculate():
+    operation = input('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtracion
+    * for multiplication
+    / for division
+    ''')
 
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
 
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1+number_2)
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1-number_2)
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1*number_2)
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1/number_2)
-else:
-    print('You have not typed a valid operator, please run the program')
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1+number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1-number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1*number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1/number_2)
+    else:
+        print('You have not typed a valid operator, please run the program')
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y to YES and N for NO.
+    ''')
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
