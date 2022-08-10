ba996b7ffbd5fa353fe6ffc60cb9f1c198643c3f
Hedelvan <hedelvan.sitema@gmail.com>
2022-08-09 19:47:09 -0500

    Update calculadora.py
    closed #3

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,25 @@
-number_1 = int(input('Enter your fisrt number: '))
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtracion
+* for multiplication
+/ for division
+''')
+
+number_1 = int(input('Enter your first number: '))
 number_2 = int(input('Enter your second number: '))
 
-print('{} + {} = '.format(number_1, number_2))
-print(number_1+number_2)
-
-#Subtraction
-
-print('{} - {} = '.format(number_1, number_2))
-print(number_1-number_2)
-
-#Mutiplication
-print('{} * {} = '.format(number_1, number_2))
-print(number_1*number_2)
-
-#Division
-print('{} / {} = '.format(number_1, number_2))
-print(number_1/number_2)
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1+number_2)
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1-number_2)
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1*number_2)
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1/number_2)
+else:
+    print('You have not typed a valid operator, please run the program')
